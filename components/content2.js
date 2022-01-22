0f167f29dc4af31b701328333dfc25f236125c75
GuilhermeTauil <guilhermetauil2@outlook.com>
2022-01-22 06:53:35 -0300

    Bug resolved!
    Quote

--- components/content2.js
+++ components/content2.js
@@ -3,9 +3,9 @@ import styles from '../styles/content2.module.css'
 export default function content2() {
     return (
         <div className={styles.Main}>
-            <p>" Olá, seja bem vindo! Meu nome é Guilherme Tauil projetei esse síte e todas as redes socias deste pequeno negócio.
+            <p> Olá, seja bem vindo! Meu nome é Guilherme Tauil projetei esse síte e todas as redes socias deste pequeno negócio.
                  Sou um profissional de confiança, com experiência no mercado de manutenção de eletroeletrônicos variados com foco em smartphones e tabletes.
-                 Entre em contato diretamento comigo clicando em qualquer link acima! "</p>
+                 Entre em contato diretamento comigo clicando em qualquer link acima! </p>
         </div>
     )
 }
