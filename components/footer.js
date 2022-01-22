cee819cad60210437c945376d67a5fec3ba3cf7f
GuilhermeTauil <guilhermetauil@outlook.com>
2022-01-22 07:43:06 -0300

    add new section

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+import styles from "../styles/footer.module.css"
+
+export default function footer(){
+    return(
+        <div className={styles.main}>
+            <h2>Garantia de serviço e de tela!</h2>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 import Header from '../components/header'
 import Content from '../components/content'
 import Content2 from '../components/content2'
-
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
@@ -14,8 +14,7 @@ export default function Home() {
       <Header/>
       <Content/>
       <Content2/>
-      <Content2/>
-
+      <Footer/>
     </div>
   )
 }
